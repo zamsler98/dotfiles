97fbc618bd688c9c9c2a7b299fb261e7f43b50b9
Zach Amsler <zachary.amsler@citizensbank.com>
2025-07-29 17:36:34 -0500

    Add mapping for opening terminal window

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,1 +1,13 @@
-return {'akinsho/toggleterm.nvim', version = "*", config = true}
+return {
+  'akinsho/toggleterm.nvim',
+  version = "*",
+  config = function()
+    require('toggleterm').setup{
+      direction = 'float',
+      open_mapping = [[<C-\>]],
+      float_opts = {
+        border = 'curved',
+      },
+    }
+  end
+}
