8966c1825b9a39105f8d8b46f2e05531fd69dbf6
Zach Amsler <zamsler98@gmail.com>
2025-12-03 14:36:12 -0600

    Remove old code

--- lua/plugins/nvim-lspconfig.lua
+++
@@ -1,7 +0,0 @@
-return {
-    "neovim/nvim-lspconfig",
-    config = function()
-        local lspconfig = require('lspconfig')
-        lspconfig.sourcekit.setup {}
-    end,
-}
--- lua/plugins/telescope.lua
+++
@@ -1,9 +0,0 @@
-return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
-      dependencies = { 'nvim-lua/plenary.nvim', 'nvim-treesitter/nvim-treesitter' },
-      opts = {
-	      defaults = {
-		      path_display = { 'truncate' },
-	      }
-      }
-}
