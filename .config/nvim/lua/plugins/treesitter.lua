9a38dcb8d6c3c81bcb77ee44ddf7bee677866436
Zach Amsler <zamsler98@gmail.com>
2025-12-08 16:27:37 -0600

    Remove jsonc from treesitter config

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -13,7 +13,6 @@ return {
                     "javascript",
                     "jsdoc",
                     "json",
-                    "jsonc",
                     "kotlin",
                     "lua",
                     "luadoc",
