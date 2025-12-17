efc68a65e51d6cbdcbf26bb3f57686e76918a37a
Zach Amsler <zamsler98@gmail.com>
2025-12-16 21:22:11 -0600

    Add clipboard support for wsl

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -37,3 +37,17 @@ vim.lsp.enable('roslyn_ls')
 vim.cmd [[highlight! link NormalFloat Normal]]
 vim.cmd [[highlight! link FloatBorder Normal]]
 
+if vim.fn.has('wsl') == 1 then
+    vim.g.clipboard = {
+        name = 'WslClipboard',
+        copy = {
+            ['+'] = 'clip.exe',
+            ['*'] = 'clip.exe',
+        },
+        paste = {
+            ['+'] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+            ['*'] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+        },
+        cache_enabled = 0,
+    }
+end
