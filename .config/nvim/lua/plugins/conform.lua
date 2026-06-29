6a35baea4d1b268d6c10d046869eef31249ed7ec
Amsler, Zach <Zachary.Amsler@citizensbank.com>
2026-06-29 15:14:20 -0500

    Fix conform running formatting twice

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -17,7 +17,7 @@ return {
                 },
                 format_on_save = {
                     timeout_ms = 2000,
-                    lsp_fallback = true,
+                    lsp_fallback = false,
                 },
             })
         end,
